9070121f075012129a1876a74b30840550319684
bravealltime <bravealltime1997@gmail.com>
2026-01-29 01:36:58 +0700

    Update ComplaintChart.tsx

--- components/dashboard/ComplaintChart.tsx
+++ components/dashboard/ComplaintChart.tsx
@@ -19,7 +19,8 @@ const ComplaintChart = ({ data }: ComplaintChartProps) => {
   ];
 
   const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const renderCustomizedLabel = (props: any) => {
+    const { cx, cy, midAngle, innerRadius, outerRadius, percent } = props;
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
